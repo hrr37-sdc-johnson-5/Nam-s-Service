e7399374210263c99ad1dfdba1d7ff541ff2411b
Druthi <polisettydruthi.k@gmail.com>
2019-04-22 19:01:10 -0700

    Refactored datagenerator to handle promises returned by writer.write function and call the write function only upon resolving the promise

--- db/data-generator.js
+++ db/data-generator.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var csvWriter = require('csv-write-stream');
 var writer = csvWriter();
 var generateTracks = require('./tracks-generator.js');
+let id = 0;
 
 var getData = () => {
   let str = [0, 1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 , 14, 15];
@@ -14,16 +15,24 @@ var getData = () => {
   };
 };
 
+var generateAlbumsInChunks = () => {
+  if(id < 10000000){
+    var data = getData();
+    data.id = id;
+    id++;
+    writer.write(data, () => {
+      generateAlbumsInChunks();
+    });
+  }else{
+    console.timeEnd('timing seed');
+    writer.end();
+  }
+}
+
 var generateAlbums = () => {
   console.time('timing seed');
-  writer.pipe(fs.createWriteStream('albums.csv'))
-  for(let i = 0; i<10000000; i++){
-    var data = getData();
-    data.id = i;
-    writer.write(data);
-  }
-  writer.end();
-  console.timeEnd('timing seed');
+  writer.pipe(fs.createWriteStream(`albums.csv`))
+  generateAlbumsInChunks();
 }
 
 generateAlbums();
