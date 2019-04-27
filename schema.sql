5397dccc787b0f671f2fff9017abc5663d9a6b76
Druthi <polisettydruthi.k@gmail.com>
2019-04-27 08:53:04 -0700

    Written all CRUD functionalities as well as modified the schema to have a SERIAL property that auto increments

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ create database media_player;
 \connect media_player;
 
 CREATE TABLE albums (
-    id INT primary key,
+    id SERIAL primary key,
     artist varchar,
     albumTitle varchar,
     album varchar,
@@ -12,11 +12,11 @@ CREATE TABLE albums (
 
 CREATE TABLE tracks (
       track varchar,
-      track_id INT primary key,
+      id SERIAL primary key,
       url varchar,
       lyrics varchar
 );
 
-\COPY albums(artist, albumTitle, album, artistDescription, id) FROM '/Users/druthipolisetty/Documents/Hack Reactor/systems-design-phase/Nam-s-Service/albums.csv' DELIMITER ',' CSV HEADER;
-\COPY tracks(track, track_id, url, lyrics) FROM '/Users/druthipolisetty/Documents/Hack Reactor/systems-design-phase/Nam-s-Service/tracks.csv' DELIMITER ',' CSV HEADER;
+\COPY albums(artist, albumTitle, album, artistDescription) FROM '/Users/druthipolisetty/Documents/Hack Reactor/systems-design-phase/Nam-s-Service/albums.csv' DELIMITER ',' CSV HEADER;
+\COPY tracks(track, url, lyrics) FROM '/Users/druthipolisetty/Documents/Hack Reactor/systems-design-phase/Nam-s-Service/tracks.csv' DELIMITER ',' CSV HEADER;
 
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('newrelic');
 var express = require ('express');
 var path = require ('path');
 var DIST_DIR = path.join(__dirname, '../client/dist');
@@ -22,7 +23,47 @@ app.get('/media/:id', (req,res) =>{
   console.time('get Data');
   db.getData(id, (err, data) => {
     console.timeEnd('get Data');
+    if(err) {
+      res.sendStatus(404);
+    }else{
+      res.status(200).json(data);
+    }
+  });
+});
+
+app.post('/media', (req, res) =>{
+  console.time('post Data');
+  let album = req.body;
+  db.createData(album, (err, data) => {
+    console.timeEnd(`post data`)
+    if(err) {
+      res.status(400);
+    }else{
+      res.status(201).json(data);
+    }
+  });
+});
+
+app.put('/media/:id', (req,res) =>{
+  var id = req.params.id;
+  let album = req.body;
+  console.time('put Data');
+  db.updateData(id, album, (err, data) => {
+    console.timeEnd('put Data');
     res.json(data);
+  })
+});
+
+app.delete('/media/:id', (req,res) =>{
+  var id = req.params.id;
+  console.time('delete Data');
+  db.deleteData(id, (err, data) => {
+    console.timeEnd('delete Data');
+    if(err) {
+      res.sendStatus(404);
+    }else{
+      res.status(200).json(data);
+    }
   })
 });
 
